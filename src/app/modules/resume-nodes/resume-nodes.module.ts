2924bb45a26f3a5d9320a5a4b57ad24536fe366d
rpfreire1 <rpfreire1@espe.edu.ec>
2023-05-24 16:13:31 -0500

    added sakai layout

---
+++ src/app/modules/resume-nodes/resume-nodes.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ResumeNodesComponent } from './resume-nodes.component';
+
+
+
+@NgModule({
+  declarations: [
+    ResumeNodesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ResumeNodesModule { }
