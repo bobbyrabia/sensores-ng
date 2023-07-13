e8273f4a1e73c4e95a950d50a9d7b2e582cc64ad
bobbyrabia <rpfreire1@espe.edu.ec>
2023-07-13 18:38:55 -0500

    logins and temperature service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,63 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { NotfoundComponent } from './demo/components/notfound/notfound.component';
-import { AppLayoutComponent } from "./layout/app.layout.component";
+import { NotfoundComponent } from './core/components/notfound/notfound.component';
+import { AppLayoutComponent } from './layout/app.layout.component';
 
 @NgModule({
     imports: [
-        RouterModule.forRoot([
+        RouterModule.forRoot(
+            [
+                {
+                    path: '',
+                    component: AppLayoutComponent,
+                    children: [
+                        {
+                            path: '',
+                            loadChildren: () =>
+                                import('./modules/general/general.module').then(
+                                    (m) => m.GeneralModule
+                                ),
+                        },
+                        {
+                            path: 'node',
+                            loadChildren: () =>
+                                import(
+                                    './modules/sensors/node/node.module'
+                                ).then((m) => m.NodeModule),
+                        },
+                        {
+                            path: 'drone',
+                            loadChildren: () =>
+                                import('./modules/drone/drone.module').then(
+                                    (m) => m.DroneModule
+                                ),
+                        },
+                    ],
+                },
+                {
+                    path: 'auth',
+                    loadChildren: () =>
+                        import('./core/components/auth/auth.module').then(
+                            (m) => m.AuthModule
+                        ),
+                },
+                {
+                    path: 'landing',
+                    loadChildren: () =>
+                        import('./core/components/landing/landing.module').then(
+                            (m) => m.LandingModule
+                        ),
+                },
+                { path: 'notfound', component: NotfoundComponent },
+                { path: '**', redirectTo: '/notfound' },
+            ],
             {
-                path: '', component: AppLayoutComponent,
-                children: [
-                    { path: '', loadChildren: () => import('./modules/general/general.module').then(m => m.GeneralModule) },
-                    { path: 'node', loadChildren: () => import('./modules/sensors/node/node.module').then(m => m.NodeModule) },
-                    { path: 'drone', loadChildren: () => import('./modules/drone/drone.module').then(m => m.DroneModule) },
-                ]
-            },
-            { path: 'auth', loadChildren: () => import('./demo/components/auth/auth.module').then(m => m.AuthModule) },
-            { path: 'landing', loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule) },
-            { path: 'notfound', component: NotfoundComponent },
-            { path: '**', redirectTo: '/notfound' },
-        ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
+                scrollPositionRestoration: 'enabled',
+                anchorScrolling: 'enabled',
+                onSameUrlNavigation: 'reload',
+            }
+        ),
     ],
-    exports: [RouterModule]
+    exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
