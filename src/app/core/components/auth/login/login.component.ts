0167ea7232c307cf2a48864a582e2f0b2e556cbe
Roberto Freire <79052020+bobbyrabia@users.noreply.github.com>
2023-07-19 15:19:56 -0500

    Revert "login test"
    This reverts commit 6b8200b05330409bade80bb54f5589b1dab4a4eb.

--- src/app/core/components/auth/login/login.component.ts
+++ src/app/core/components/auth/login/login.component.ts
@@ -1,39 +1,23 @@
 import { Component } from '@angular/core';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
-import { Router } from '@angular/router';
-import { NgForm } from '@angular/forms';
-import { User } from 'src/app/types/user.types';
-import { TemperatureService } from '../../../http/temperature.service';
-import { AuthService } from '../../../http/auth.service';
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
-    styles: [
-        `
-            :host ::ng-deep .pi-eye,
-            :host ::ng-deep .pi-eye-slash {
-                transform: scale(1.6);
-                margin-right: 1rem;
-                color: var(--primary-color) !important;
-            }
-        `,
-    ],
+    styles: [`
+        :host ::ng-deep .pi-eye,
+        :host ::ng-deep .pi-eye-slash {
+            transform:scale(1.6);
+            margin-right: 1rem;
+            color: var(--primary-color) !important;
+        }
+    `]
 })
 export class LoginComponent {
-    user: User = {
-        email: '',
-        password: '',
-    };
-    constructor(
-        public layoutService: LayoutService,
-        private router: Router,
-        private authService: AuthService
-    ) {}
-    login(form: NgForm) {
-        console.log('form value', form.value);
-        this.authService.login(this.user).subscribe((response) => {
-            this.router.navigate(['/']);
-        });
-    }
+
+    valCheck: string[] = ['remember'];
+
+    password!: string;
+
+    constructor(public layoutService: LayoutService) { }
 }
